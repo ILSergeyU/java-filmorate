ce34c7b7269dcddc0f67e93ee8bd7bfef2e549e0
SergeyIU <Murmansk.axsel@yandex.ru>
2024-01-21 14:17:54 +0300

    Test

---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -0,0 +1,20 @@
+package ru.yandex.practicum.filmorate.storage;
+
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.util.List;
+
+
+public interface FilmStorage {
+
+
+    List<Film> finAll();
+
+    Film movieById(int id);
+
+    Film createFilm(Film film);
+
+    Film update(Film film);
+
+
+}
