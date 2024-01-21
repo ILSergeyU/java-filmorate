95a95de685a75414ae521375f88ecf387ee8c1bc
SergeyIU <Murmansk.axsel@yandex.ru>
2024-01-21 14:17:44 +0300

    Test

--- src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
@@ -1,11 +1,8 @@
 package ru.yandex.practicum.filmorate.storage;
 
-import lombok.Getter;
 import ru.yandex.practicum.filmorate.model.User;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public interface UserStorage {
 
